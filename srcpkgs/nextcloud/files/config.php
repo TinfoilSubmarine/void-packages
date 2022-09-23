0c77c9e6b9d63fe259c88fa4dfda2080b9aab480
Joel Beckmeyer <joel@beckmeyer.us>
2022-09-23 19:18:57 -0400

    New package: nextcloud-33.0.5

---
+++ srcpkgs/nextcloud/files/config.php
@@ -0,0 +1,16 @@
+<?php
+$CONFIG = array (
+'datadirectory' => '/var/lib/nextcloud/data',
+'apps_paths' => [
+  [
+    'path'=> '/usr/share/webapps/nextcloud/apps',
+    'url' => '/apps',
+    'writable' => false,
+  ],
+  [
+    'path'=> '/var/lib/nextcloud/apps',
+    'url' => '/wapps',
+    'writable' => true,
+  ],
+],
+);
